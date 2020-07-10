1c62960b42b836543cd68b014654a9545f8688c1
Yoselyn Carolina <ycarolina692@gmail.com>
2020-07-10 17:05:44 -0400

    Carga de datos mediante CSV

--- src/app/services/csv2fire.service.ts
+++ src/app/services/csv2fire.service.ts
@@ -1,37 +1,73 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import * as Papa from 'papaparse';
 import * as _ from 'lodash';
+import { UserI } from '../models/user.interface';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class Csv2fireService {
 
+  
+  private usersCollection: AngularFirestoreCollection<UserI>;
+
+  
   csv_rec: any[] = [];
   header = false;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore,private _snackBar: MatSnackBar) { }
 
   process(file, collection) {
     Papa.parse(file, {
       complete: res => {
-        this.csv_rec = res;
-        this.firethis(this.csv_rec['data'], collection);
+      this.csv_rec = res;
+      this.firethis(this.csv_rec['data'], collection);
       },
       header: true
     });
   }
 
+  /* Si el documento no existe, se creará. Si el documento existe, 
+  su contenido se sobrescribirá con los datos proporcionados. */
   firethis(json, collection) {
+    console.log(json);
+    json = this.tamanioJson(json);
+    console.log(json);
+    let idDoc = '';
     return new Promise((resolve) => {
-      _.map(json, (e, i) => {
-        _.keys(e).map(() => {
-          this.afs.collection(collection).doc(i).set(e);
+        _.map(json, (e, i) => {
+          _.keys(e).map(() => {
+            idDoc = (e.id_usuario).toString();
+            this.afs.collection("com_usuario").doc(idDoc).set(e)
+          .then(function() {
+            console.log("Document successfully written!");
+          })
+          .catch(function(error) {
+            console.error("Error writing document: ", error);
+          });
+          })
         })
-      })
-      resolve();
+        resolve();
     })
   }
+
+  tamanioJson(json){
+    let usersJson :any[] =[];
+    for (let index = 0; index < json.length-1; index++) {
+      usersJson.push(json[index]);
+    }
+    return usersJson;
+  }
+
+  openSnackBar(mensaje : string) {
+    this._snackBar.open(mensaje, "OK", {
+      duration: 5000,
+    });
+  }
+
+  
 
 }
--- src/app/userupload/userupload.component.ts
+++ src/app/userupload/userupload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component} from '@angular/core';
 import { Csv2fireService } from '../services/csv2fire.service';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { Observable } from 'rxjs';
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
   templateUrl: './userupload.component.html',
   styleUrls: ['./userupload.component.scss']
 })
-export class UseruploadComponent implements OnInit {
+
+export class UseruploadComponent {
 
   message = 'Uploading';
   showMessage: boolean = false;
@@ -20,30 +21,20 @@ export class UseruploadComponent implements OnInit {
   isUploading: boolean;
   isUploaded: boolean;
 
-
-  
-
-  constructor(private storage: AngularFireStorage, private csv2fire: Csv2fireService) {}
-
-  
-  ngOnInit(): void {
-    this.isUploading = false;
-    this.isUploaded = false;
+  constructor(private storage: AngularFireStorage, private csv2fire: Csv2fireService) {
+  this.isUploading = false;
+  this.isUploaded = false;
   }
 
-
-  
   startUpload(event: FileList) {
     const file = event.item(0);
     if(file.type.split('/')[1] !== 'csv') {
-    console.error('Unsupported file type!!');
+      console.error('Tipo de archivo no soportado!!');
     }
     this.isUploading = true;
     this.isUploaded = false;
     this.csv2fire.process(file, 'data');
+    
   }
-
-
-  
 
 }
